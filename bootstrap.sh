6321958f0b0e42fb164bfcb27ded8812aaf175be
Vinicius Mucuge <vinicius.mucuge@gmail.com>
2015-02-03 18:02:12 +0100

    Fixes to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
-set -e
+set -x
 
 echo "Running into vagrant"
 # sudo yum localinstall -y /vagrant/rdo-release-icehouse-4.noarch.rpm
 
-sudo yum update -y
-
-# PUPPET is already installed on this image
-# sudo yum install puppet -y
+yum update -y
 
 if [ ! -d /root/.ssh ] ; then
-  sudo mkdir /root/.ssh
+  mkdir -p /root/.ssh
 fi
 
 sudo cp /vagrant/files/id_rsa* /root/.ssh/
 sudo chmod 600 /root/.ssh/id_rsa
 sudo chmod 644 /root/.ssh/id_rsa.pub
-sudo cat /root/.ssh/id_rsa.pub > /root/.ssh/authorized_keys
+#sudo chmod 777 /root/.ssh/
+sudo cp /vagrant/files/authorized_keys /root/.ssh/authorized_keys
+sudo chmod 750 /root/.ssh/
 
-pvs |grep cinder-volumes > /dev/null
-if [ $? -ne 0 ];then
-  sudo pvcreate  /dev/sdb
-  sudo vgcreate cinder-volumes /dev/sdb
+vgs | grep cinder-volumes > /dev/null
+if [ $? -ne 0 ] ; then
+  pvcreate  /dev/sdb
+  vgcreate cinder-volumes /dev/sdb
 fi
 
-sudo packstack --answer-file=/vagrant/packstack_answers.txt
+packstack --answer-file=/vagrant/packstack_answers.txt
